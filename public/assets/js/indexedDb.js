560ef1c90696f2c2cc3b8d7363b4981921a003d9
Matthew Henry <tadaemusic@gmail.com>
2020-07-17 18:09:03 -0400

    updated database & added manifest

--- public/assets/js/indexedDb.js
+++ public/assets/js/indexedDb.js
@@ -1,25 +1,55 @@
-
 let db;
+
 const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = function(e) {
-    const db = e.target.result;
-    db.createObjectStore("pending", { autoIncrement: true });
-  };
+request.onupgradeneeded = function (e) {
+  const db = e.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
 
-  request.onsuccess = function (e) {
-    db = e.target.result;
-  
-    // scan database on successful server connection
-    if (navigator.onLine) {
-      scanDatabase();
+request.onsuccess = function (e) {
+  db = e.target.result;
+
+  // scan database on successful server connection
+  if (navigator.onLine) {
+    scanDatabase();
+  }
+};
+
+request.onerror = function (e) {
+  console.log("Error: " + e.target.errorCode);
+};
+
+function recordData(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+}
+
+function scanDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const fetchAll = store.fetchAll();
+
+  fetchAll.onsuccess = function () {
+    if (fetchAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(fetchAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
     }
   };
+}
 
-  request.onerror = function (e) {
-    console.log("Error: " + e.target.errorCode);
-  };
-  
-  
-
-
+window.addEventListener("online", scanDatabase);
--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,11 @@
-const CACHE_FILE = [
-    "/",
-    "/index.html",
-    "index.js",
-    "/db.js",
-    "/style.css"
+const CACHE_FILES = [
+  "/",
+  "/index.html",
+  "/assets/js/index.js",
+  "/assets/js/indexedDb.js",
+  "/assets/css/styles.css",
+  "/manifest.json",
+  "/sw.js",
 ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -12,21 +14,61 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 // install cache files
 self.addEventListener("install", function (e) {
-    
-    e.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache) {
-        console.log("Opened cache");
-        return cache.addAll(CACHE_FILE);
-      })
-    );
-  });
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(CACHE_FILES);
+    })
+  );
 
-  self.addEventListener('fetch', function(e) {
-    console.log(e.request.url);
-   
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", (e) => {
+  if (e.request.url.includes("/api/")) {
+    console.log("[Service Worker] Fetch(data)", e.request.url);
+
     e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(e.request)
+          .then((response) => {
+            if (response.status === 200) {
+              cache.put(e.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch((err) => {
+            return cache.match(e.request);
+          });
       })
     );
-   });
+    return;
+  }
+
+  e.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(e.request).then((response) => {
+        return response || fetch(e.request);
+      });
+    })
+  );
+});
